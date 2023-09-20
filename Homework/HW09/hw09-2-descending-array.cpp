02fd3bd12e97bb727b189176674ef1a0c256819a
natthakit111 <natthakit_ya66@live.ac.th>
2023-09-20 20:46:12 +0700

    update hw9-2

--- Homework/HW09/hw09-2-descending-array.cpp
+++ Homework/HW09/hw09-2-descending-array.cpp
@@ -23,3 +23,62 @@
         99-0 : 13 9 6 5 4 1 1
 
 */
+
+#include <stdio.h>
+
+int main() {
+    int a[100] ;
+    int n = 0 ;    
+    int p ;
+
+    while ( 1 ) {
+        printf( "Input : ") ;
+        scanf( "%d", &p ) ;
+
+        if ( p  == -1 ) {
+            break ;
+        }//end if
+
+        if ( n < 100 ) {
+            a[n] = p ;
+            n++ ;
+        } else {
+            printf( "อาเรย์เต็มแล้ว\n" ) ;
+            break ;
+        }//end if
+    }//end while
+
+    for ( int i = 0 ; i < n - 1 ; i++ ) {
+        for ( int j = i + 1 ; j < n ; j++ ) {
+            if ( a[i] > a[j] ) {
+                int temp = a[i] ;
+                a[i] = a[j] ;
+                a[j] = temp ;
+            }//end if
+        }//end for
+    }//end for
+
+    printf( "0-99 : " ) ;
+    for ( int i = 0 ; i < n ; i++ ) {
+        printf( "%d ", a[i] ) ;
+    }//end for
+    printf( "\n" ) ;
+ 
+    for ( int i = 0 ; i < n - 1 ; i++ ) {
+        for ( int j = i + 1 ; j < n ; j++ ) {
+            if ( a[i] < a[j] ) {
+                int temp = a[i] ;
+                a[i] = a[j] ;
+                a[j] = temp ;
+            }//end if
+        }//end for
+    }//end for
+    printf( "99-0 : " ) ;
+    for ( int i = 0 ; i < n ; i++ ) {
+        printf( "%d ", a[i] ) ;
+    }//end for
+    printf( "\n" ) ;
+
+
+    return 0 ;
+}//end function
